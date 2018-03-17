1d59944b09a2e71269715d86fed7dcc62b76c6b1
Wellington Barbosa <wellington.barbosa@nextel.com.br>
2018-03-17 15:56:16 -0300

    Update App.js

--- PlaceYourOrder/App.js
+++ PlaceYourOrder/App.js
@@ -1,23 +1,97 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, View, StatusBar,
+  ToolbarAndroid, Platform, Text
+} from 'react-native';
+import { Constants } from 'expo';
+import * as LoginReducers from './src/reducers/LoginReducers';
+import * as LoadingReducers from './src/reducers/LoadingReducers';
+import * as ErrorReducers from './src/reducers/ErrorReducers';
+import { HomeTabNavigator } from './src/components/HomeTabNavigator';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { StackNavigator } from 'react-navigation'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Font } from 'expo';
+import LoginScreen from './src/components/screens/LoginScreen';
+import ProductListScreen from './src/components/screens/ProductListScreen';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const loginReducer = LoginReducers.reducer;
+const loadingReducer = LoadingReducers.reducer;
+const errorReducer = ErrorReducers.reducer;
+
+const configureStore = () => {
+  const store = createStore(
+    combineReducers({ loginReducer, loadingReducer, errorReducer }),
+    composeEnhancers(
+      applyMiddleware(logger, thunk)
+    )
+  );
+  return store;
+};
+
+const CustomStatusBar = ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+};
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: LoginScreen,
+    navigationOptions: {
+      title: 'PlaceYourOrder',
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'blue',
+      }
+    }
+  },
+  ProductListScreen: {
+    screen: HomeTabNavigator,
+    navigationOptions: {
+      title: 'PlaceYourOrder',
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'blue',
+      }
+    }
+  },
+  ProductListScreen2: {
+    screen: ProductListScreen
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={configureStore()}>
+        <View style={styles.container}>
+          <CustomStatusBar backgroundColor={'blue'} barStyle="light-content" />
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
     justifyContent: 'center',
   },
+  autocompleteContainer: {
+    flex: 1,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    zIndex: 1
+  }
 });
+
